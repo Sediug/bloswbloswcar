6fb439a491a2a3e1367aa382c5c19bfe8b27232a
sebastiancbvz <sebastian.cabanas@cbiconsulting.es>
2018-03-04 01:26:39 +0100

    Change var names in api

--- server/routes/api.js
+++ server/routes/api.js
@@ -10,8 +10,11 @@ const api = new Route(
 	gw => {
 		// api info
 		gw.json({
-			announcements: '/api/announcements/:id',
-			users: '/api/users/:id'
+			description: 'API Rest v1',
+			endPoints: {
+				announcements: '/api/announcements/:id',
+				users: '/api/users/:id'
+			}
 		}, {deep: 0});
 	}
 );
@@ -22,11 +25,11 @@ api.routes.add(new Route({ path:'/users', method:['get'] }, users.get));
 api.routes.add(new Route({ path:'/users/:id', method:['get'] }, users.get));
 
 // Announcements
-const an = require('../controller/announcements');
-api.routes.add(new Route({ path:'/announcements', method:['get'] }, an.get));
-api.routes.add(new Route({ path:'/announcements/:id', method:['get'] }, an.get));
-api.routes.add(new Route({ path:'/announcements/:id', method:['post'] }, an.create));
-api.routes.add(new Route({ path:'/announcements/:id', method:['put'] }, an.update));
-api.routes.add(new Route({ path:'/announcements/:id', method:['delete'] }, an.delete));
+const announcements = require('../controller/announcements');
+api.routes.add(new Route({ path:'/announcements', method:['get'] }, announcements.get));
+api.routes.add(new Route({ path:'/announcements/:id', method:['get'] }, announcements.get));
+api.routes.add(new Route({ path:'/announcements/:id', method:['post'] }, announcements.create));
+api.routes.add(new Route({ path:'/announcements/:id', method:['put'] }, announcements.update));
+api.routes.add(new Route({ path:'/announcements/:id', method:['delete'] }, announcements.delete));
 
 module.exports =  api;
